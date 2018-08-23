7be7c27dfa034acb574d3dfee69baf930d9059ac
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-23 14:15:25 -0500

    forgot the “local” descriptor

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -14,11 +14,11 @@ data "aws_iam_policy_document" "cloudwatch_assume_role" {
 
 resource "aws_iam_role" "cloudwatch_logs" {
   assume_role_policy = "${data.aws_iam_policy_document.cloudwatch_assume_role.json}"
-  name = "${associated_resource_name}"
+  name = "${local.associated_resource_name}"
 }
 
 resource "aws_cloudwatch_log_group" "cloudtrail" {
-  name              = "${associated_resource_name}"
+  name              = "${local.associated_resource_name}"
   retention_in_days = 7
 }
 
