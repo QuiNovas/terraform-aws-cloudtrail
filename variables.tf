868af953528b919e29d013439c7c1f1591488dc7
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-06-24 19:48:49 +0530

    terraform 0.12 upgrade

--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 variable "account_name" {
   description = "The name of the account. Used as a prefix for naming resources"
-  type        = "string"
+  type        = string
 }
 
 variable "expiration" {
   default     = 2555
   description = "The number of days to wait before expiring an object"
-  type        = "string"
+  type        = string
 }
 
 variable "log_bucket" {
   description = "The log bucket to log S3 logs to."
-  type        = "string"
+  type        = string
 }
 
 variable "transition_to_glacier" {
   default     = 30
   description = "The number of days to wait before transitioning an object to Glacier"
-  type        = "string"
+  type        = string
 }
+
