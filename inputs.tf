54482c1666579f2c22fb19ba63c8c2f8b90e71b6
Chris Hurst <chris.hurst@stratusgrid.com>
2019-07-30 13:59:42 -0400

    initial changes for terraform 12 support

--- inputs.tf
+++ inputs.tf
@@ -1,33 +1,34 @@
 variable "name_prefix" {
   description = "String to prefix on object names"
-  type = "string"
+  type        = string
 }
 
 variable "name_suffix" {
   description = "String to append to object names. This is optional, so start with dash if using"
-  type = "string"
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "input_tags" {
   description = "Map of tags to apply to resources"
-  type = "map"
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "expiration" {
   default     = 2557
   description = "The number of days to wait before expiring an object"
-  type        = "string"
+  type        = string
 }
 
 variable "log_bucket" {
   description = "The log bucket to log S3 logs to."
-  type        = "string"
+  type        = string
 }
 
 variable "transition_to_glacier" {
   default     = 366
   description = "The number of days to wait before transitioning an object to Glacier"
-  type        = "string"
+  type        = string
 }
+
--- kms-key.tf
+++ kms-key.tf
@@ -1,57 +1,58 @@
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 data "aws_iam_policy_document" "cloudtrail_kms" {
   statement {
     actions = [
-      "kms:*"
+      "kms:*",
     ]
     principals {
       identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
       ]
       type = "AWS"
     }
     resources = [
-      "*"
+      "*",
     ]
     sid = "Enable IAM User Permissions"
   }
 
   statement {
     actions = [
-      "kms:GenerateDataKey*"
+      "kms:GenerateDataKey*",
     ]
     condition {
       test = "StringLike"
       values = [
-        "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
+        "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*",
       ]
       variable = "kms:EncryptionContext:aws:cloudtrail:arn"
     }
     principals {
       identifiers = [
-        "cloudtrail.amazonaws.com"
+        "cloudtrail.amazonaws.com",
       ]
       type = "Service"
     }
     resources = [
-      "*"
+      "*",
     ]
     sid = "Allow CloudTrail to encrypt logs"
   }
 
   statement {
     actions = [
-      "kms:DescribeKey"
+      "kms:DescribeKey",
     ]
     principals {
       identifiers = [
-        "cloudtrail.amazonaws.com"
+        "cloudtrail.amazonaws.com",
       ]
       type = "Service"
     }
     resources = [
-      "*"
+      "*",
     ]
     sid = "Allow CloudTrail to describe key"
   }
@@ -60,11 +61,11 @@ data "aws_iam_policy_document" "cloudtrail_kms" {
 resource "aws_kms_key" "cloudtrail" {
   description         = "cloudtrail log key for ${local.trail_name}"
   enable_key_rotation = true
-  policy              = "${data.aws_iam_policy_document.cloudtrail_kms.json}"
+  policy              = data.aws_iam_policy_document.cloudtrail_kms.json
 }
 
 resource "aws_kms_alias" "cloudtrail" {
   name          = "alias/${local.associated_resource_name}"
-  target_key_id = "${aws_kms_key.cloudtrail.key_id}"
+  target_key_id = aws_kms_key.cloudtrail.key_id
 }
 
