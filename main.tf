cc2b50bdc16b4aea2703d2ed5fea61630d50db0d
Ivan Casco <ivan.casco@stratusgrid.com>
2020-11-07 00:29:00 +0100

    Add required wildcard in cloud_watch_logs_group_arn in AWS provider >=3.0.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudtrail" "cloudtrail" {
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudwatch_logs.arn
   depends_on                    = [aws_s3_bucket_policy.cloudtrail]
   enable_log_file_validation    = true
