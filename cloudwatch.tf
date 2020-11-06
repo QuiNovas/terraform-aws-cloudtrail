9d756505e8a9f484819349aa707006dc519c8d33
Ivan Casco <ivan.casco@stratusgrid.com>
2020-11-07 00:34:04 +0100

    Add Terraform linter github action

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "cloudwatch_logs_role" {
       "logs:PutLogEvents",
     ]
     resources = [
-      aws_cloudwatch_log_group.cloudtrail.arn,
+      "${aws_cloudwatch_log_group.cloudtrail.arn}:*",
     ]
     sid = "AWSCloudTrailLogging"
   }
